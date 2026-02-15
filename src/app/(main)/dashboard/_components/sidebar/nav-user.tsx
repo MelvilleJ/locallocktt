e12a36d4d87f6d7644b58b5a127e8e4097cdd8ce
Melvillej <joshuamelville.work@gmail.com>
2026-02-14 21:42:18 -0400

    background work with clerk auth and started to prepare for database intergration with clerk

--- src/app/(main)/dashboard/_components/sidebar/nav-user.tsx
+++ src/app/(main)/dashboard/_components/sidebar/nav-user.tsx
@@ -79,7 +79,7 @@ export function NavUser() {
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <SignOutButton redirectUrl="/login">
+            <SignOutButton redirectUrl="/">
               <DropdownMenuItem className="cursor-pointer">
                 <LogOut className="mr-2 h-4 w-4" />
                 <span>Log out</span>
